b8d6908a6f8d788b167b1a037126e0a16b15832c
Shi Yan <billconan@gmail.com>
2023-02-18 14:26:14 -0800

    update

--- tags.js
+++ tags.js
@@ -1,6 +1,7 @@
 import textSchema from "./textschema";
 import { Transform, StepMap } from "prosemirror-transform"
 import { TextSelection, Selection } from "prosemirror-state"
+import { inputRules } from "prosemirror-inputrules";
 
 
 export default class TagsView {
@@ -14,7 +15,9 @@ export default class TagsView {
     this.dom = document.createElement("div");
     this.dom.classList.add('limpid-tag-area');
     this.input = document.createElement("input");
-    this.input.classList.add('limpid-custom-input');
+    this.input.classList.add('limpid-tag-input');
+    this.input.classList.add('limpid-no-outline');
+    this.input.placeholder = "Hit Enter, Tab or Comma to add a tag. Click on a tag to remove..."
     this.dom.appendChild(this.input);
     this.dom.onclick = (e) => {
       e.preventDefault();
@@ -31,29 +34,14 @@ export default class TagsView {
 
     this.input.addEventListener('keydown', (e) => {
 
-      var str = 'fake';
-
-      console.log("keydown", e.code)
-
       if (!!(~['Enter', 'Tab', 'Comma'].indexOf(e.code))) {
-        let nn = textSchema.nodes.tag.createAndFill(null, textSchema.text("fake"));
-        //let tr = new Transform(node);
+        let nn = textSchema.nodes.tag.createAndFill(null, textSchema.text(inputElm.value));
+        inputElm.value = '';
+        e.preventDefault();
 
         let tr = self.outerView.state.tr.replaceWith(getPos() + 1, getPos() + 1 + self.node.nodeSize - 2, [nn]);
-        /*console.log(tr);
-        let offsetMap = StepMap.offset(getPos() + 1);
-        let outerTr = view.state.tr;
-        let steps = tr.steps
-        for (let j = 0; j < steps.length; j++) {
-          outerTr.step(steps[j].map(offsetMap))
-        }
-        if (outerTr.docChanged)*/
+        
         self.outerView.dispatch(tr);
-
-        console.log('trigger', getPos(), self.node)
-        //  input.value = "";
-        // if(str != "")
-        //   tags.addTag(str);
       }
       else if (e.code === 'ArrowUp') {
         inputElm.blur();
@@ -85,22 +73,17 @@ export default class TagsView {
   update(node) {
     if (node.type != this.node.type) return false
     this.node = node
-    console.log('node update', node)
-
     node.forEach(
       (node, offset, index) => {
         console.log(node.textContent)
-
         let tags = this.dom.getElementsByTagName("span");
-
-
         for (let i = 0; i < tags.length; ++i) {
           this.dom.removeChild(tags[i]);
         }
 
-
         let tag = document.createElement("span");
-        tag.innerText = node.textContent;
+        tag.classList.add('limpid-tag-item');
+        tag.innerText = '#'+node.textContent ;
         this.dom.insertBefore(tag, this.dom.firstChild);
       }
     )
@@ -109,12 +92,12 @@ export default class TagsView {
 
   selectNode() {
     console.log('node selected')
-    this.dom.classList.add("ProseMirror-selectednode")
+    //this.dom.classList.add("ProseMirror-selectednode")
     this.input.focus();
   }
 
   deselectNode() {
-    this.dom.classList.remove("ProseMirror-selectednode")
+    //this.dom.classList.remove("ProseMirror-selectednode")
     this.input.blur();
   }
 
