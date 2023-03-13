cf764c955de447f84b8b10286072155df1281e7d
Shi Yan <billconan@gmail.com>
2023-03-12 22:34:22 -0700

    update readme

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -4,8 +4,8 @@ use std::process::Command;
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=Cargo.lock");
-    println!("cargo:rerun-if-changed=frontend/src");
-    println!("cargo:rerun-if-changed=frontend/package.json");
+    println!("cargo:rerun-if-changed=../frontend/src");
+    println!("cargo:rerun-if-changed=../frontend/package.json");
 
     let output = Command::new("npm")
         .current_dir("../frontend")
